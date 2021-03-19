85fa1d38cb1a619ba8b8e6c2ebf534348c4930d2
srinidh-007 <srinidhreddyyeldhandi123@gmail.com>
2021-03-19 23:01:56 +0530

    OS module

--- django_project/settings.py
+++ django_project/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = '4yazjz@x_73l7y0!om9bldkuehfuefhvfuhfuhfubcbc'
-SECRET_KEY = os.environ.get('SECURITY_KEY')
+SECRET_KEY = os.getenv('SECURITY_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
